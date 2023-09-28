1faecb92d80bfdde381efb3cd9e7ce3bb5f9c938
cynthia <118978961+cyntrist@users.noreply.github.com>
2023-09-28 20:30:52 +0200

    arreglado ej 2

--- ejercicio2/main.js
+++ ejercicio2/main.js
@@ -12,22 +12,21 @@ window.onload = function () {
         li.dataset.charaid = character.id;
         list.appendChild(li);
     });
+
+    var select = document.querySelector('select[name=chara]');
+    party.forEach(function (character) {
+        var option = document.createElement('option');
+        option.innerHTML = character.name;
+        option.value = character.id;
+        select.appendChild(option);
+    });
+    
+    var form = document.querySelector('form[name=killing-machine]');
+    form.addEventListener('submit', function (event) {
+        var list = document.getElementById('party-monsters'); // he tenido que añadir esta línea porque si no el query decía que list no estaba definida (entendible)
+        event.preventDefault();
+        var charaID = form.querySelector('[name=chara]').value;
+        var li = list.querySelector('[data-charaid=' + charaID + ']');
+        li.classList.add('dead');
+    });
 };
-
-var select = document.querySelector('select[name=chara]');
-party.forEach(function (character) {
-    var option = document.createElement('option');
-    option.innerHTML = character.name;
-    option.value = character.id;
-    select.appendChild(option);
-});
-
-var form = document.querySelector('form[name=killing-machine]');
-form.addEventListener('submit', function (event) {
-    var list = document.getElementById('party-monsters'); // he tenido que añadir esta línea porque si no el query decía que list no estaba definida (entendible)
-    event.preventDefault();
-    var charaID = form.querySelector('[name=chara]').value;
-    var li = list.querySelector('[data-charaid=' + charaID + ']');
-    li.classList.add('dead');
-});
-
