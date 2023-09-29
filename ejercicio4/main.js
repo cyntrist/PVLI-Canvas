284707d27e75bb985e0c05fc978213ff40f7fbf4
cyntrist <118978961+cyntrist@users.noreply.github.com>
2023-09-29 16:04:33 +0200

    mejroas esteticas

--- ejercicio4/main.js
+++ ejercicio4/main.js
@@ -38,18 +38,49 @@ window.onload = function () {
     party.forEach(function (char, index) {
       var x = index * charaSpace + centerOffset;
       var y;
+      let radio = 50;
+
       if (char.hp === 0) {
         context.fillStyle = 'grey';
         y = 500; // en el suelo porque está muerto.
       } else if (char.name === 'Bat') {
         context.fillStyle = 'blue';
+        //y = 400;
         y = 50 * Math.sin(t/100) + 300; // flotando en el aire.
+
+        radio = 40 // alas
+        context.beginPath();
+        context.moveTo(x, y);
+        context.lineTo(x-100,y);
+        context.lineTo(x-50, y-50);
+        context.lineTo(x,y);
+        context.lineTo(x+100,y);
+        context.lineTo(x+50, y-50);
+        context.fill();
       } else if (char.name === 'Slime') {
         context.fillStyle = 'green';
         y = 400; // en el suelo pero no en la tumba.
+
+        context.beginPath(); // ...pies?
+        context.moveTo(x + 30, y - 40);
+        context.lineTo(x - 30, y - 40);
+        context.lineTo(x - 70, y + 50);
+        context.lineTo(x + 70, y + 50);
+        context.fill();
+
       }
+
       context.beginPath();
-      context.arc(x, y, 50, 0, 2 * Math.PI);
+      context.arc(x, y, radio, 0, 2 * Math.PI);
+      context.fill();
+
+      context.fillStyle = "red"; // ojos
+      context.beginPath();
+      context.ellipse(x - 10, y, 10, 5, Math.PI / 2, 0, 2 * Math.PI);
+      context.fill();
+
+      context.beginPath();
+      context.ellipse(x + 10, y, 10, 5, Math.PI / 2, 0, 2 * Math.PI);
       context.fill();
     });
   }
@@ -105,18 +136,20 @@ window.onload = function () {
     var character = findCharById(charaID);
     let damage = 5;
     let i = 0;
-    function callback() { // bucle: frame por unidad de daño
-      character.hp--;
-      i++;
-      if (i < damage) {
-        window.requestAnimationFrame(callback);
-      }
-    }
-    window.requestAnimationFrame(callback);
     if (character.hp <= 0) {
       character.hp = 0; // corrige el valor en caso de que sea negativo.
       var li = list.querySelector('[data-charaid=' + charaID + ']');
       li.classList.add('dead');
+    }
+    else {
+      function callback() { // bucle: frame por unidad de daño
+        character.hp--;
+        i++;
+        if (i < damage) {
+          window.requestAnimationFrame(callback);
+        }
+      }
+      window.requestAnimationFrame(callback);
     }
   });
   
