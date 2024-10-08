8f78d182abb6154a58e99b8d89717cac9ed6bf6c
cyntrist <cyntrist@ucm.es>
2024-10-08 16:29:48 +0200

    butones

--- ejercicio2/main.js
+++ ejercicio2/main.js
@@ -1,7 +1,7 @@
 var party = [
-    {name: 'Bat', id: 'bat1'},
-    {name: 'Slime', id: 'slime'},
-    {name: 'Bat', id: 'bat2'}
+    { name: 'Bat', id: 'bat1' },
+    { name: 'Slime', id: 'slime' },
+    { name: 'Bat', id: 'bat2' }
 ];
 
 window.onload = function () {
@@ -13,19 +13,33 @@ window.onload = function () {
         list.appendChild(li);
     });
 
+    let but = document.getElementById('boton');
     var select = document.querySelector('select[name=chara]');
+    select.addEventListener('change', function (event) {
+        event.preventDefault();
+        var charaID = form.querySelector('[name=chara]').value;
+        var li = list.querySelector('[data-charaid=' + charaID + ']');
+        if (li.classList.contains('dead')) {
+            but.disabled = true;
+        }
+        else {
+            but.disabled = false;
+        }
+    });
+
     party.forEach(function (character) {
         var option = document.createElement('option');
         option.innerHTML = character.name;
         option.value = character.id;
         select.appendChild(option);
     });
-    
+
     var form = document.querySelector('form[name=killing-machine]');
     form.addEventListener('submit', function (event) {
         event.preventDefault();
         var charaID = form.querySelector('[name=chara]').value;
         var li = list.querySelector('[data-charaid=' + charaID + ']');
         li.classList.add('dead');
+        but.disabled = true;
     });
 };
