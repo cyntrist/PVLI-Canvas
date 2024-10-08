977b80c2e18df54b8205dc19ef16c72f42b334ce
cyntrist <cyntrist@ucm.es>
2024-10-08 15:55:34 +0200

    lo de los gatos

--- ejercicio1/main.js
+++ ejercicio1/main.js
@@ -1,8 +1,11 @@
 window.onload = function () {
     var button = document.getElementById('summon');
     button.addEventListener('click', function () {
-        var img = new Image();
-        img.src = 'https://placekitten.com/g/200/200/';
-        document.getElementById('kittens').appendChild(img);
+        // var img = new Image();
+        // img.src = 'https://placekitten.com/g/200/200/';
+        // document.getElementById('kittens').appendChild(img);
+
+        const list = document.getElementById("kittens");
+        list.innerHTML += `<img src="https://placekitten.com/g/200/200/"/>`;
     });
 };
